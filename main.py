f5d78bdf7576871a489449516c3c9c0afe6a1c54
enchance <enchance@gmail.com>
2021-01-17 15:57:56 +0800

    Add localhost to CORS

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def get_app():
     )
 
     # CORS
-    origins = ['*']
+    origins = ['http://localhost:3000', 'http://127.0.0.1:3000']
     app.add_middleware(
         CORSMiddleware, allow_origins=origins, allow_credentials=True,
         allow_methods=["*"], allow_headers=["*"],
