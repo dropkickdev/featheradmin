b0d46d5b93bf40d7ad4f2848e85b995c7295c515
enchance <enchance@gmail.com>
2021-01-22 19:46:30 +0800

    Test for wrong login

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -8,8 +8,8 @@ def test_register(client, random_email):
     assert res.status_code == 201
 
 # @pytest.mark.skip
-@pytest.mark.focus
-def test_login(client):
+# @pytest.mark.focus
+def test_login_existent(client):
     d = dict(username='protective@yahoo.co.uk', password='pass123')
     res = client.post('/auth/login', data=d)
     assert res.status_code == 200
@@ -17,3 +17,9 @@ def test_login(client):
     # except Exception as e:
     #     ic(type(e))
     #     raise e
+
+@pytest.mark.focus
+def test_login_nonexistent(client):
+    d = dict(username='aaa@bbb.com', password='pass123')
+    with pytest.raises(AttributeError):
+        client.post('/auth/login', data=d)
