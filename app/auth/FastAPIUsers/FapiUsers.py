f12bcf6d359408beb7572fee7db20d5e92e4b9e5
enchance <enchance@gmail.com>
2021-02-24 23:03:54 +0800

    Create draft for the FapiUsers class
    Prev commit

---
+++ app/auth/FastAPIUsers/FapiUsers.py
@@ -0,0 +1,20 @@
+from typing import Callable, Optional
+
+from fastapi import APIRouter
+from fastapi_users import models
+from fastapi_users.authentication import Authenticator, BaseAuthentication
+from fastapi_users.fastapi_users import FastAPIUsers
+from starlette.requests import Request
+
+from app.auth.FastAPIUsers.Routers import Routers
+from app.auth.FastAPIUsers.user import get_get_user
+
+
+REFRESH_TOKEN_KEY = 'refresh_token'
+RESET_PASSWORD_TOKEN_AUDIENCE = "fastapi-users:reset"
+
+
+class FapiUsers(FastAPIUsers):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.get_user = get_get_user(self.db)
