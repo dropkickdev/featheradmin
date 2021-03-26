87ee8b411083d5a1dcb20c88a4a850dc9e2d30b7
enchance <enchance@gmail.com>
2021-03-26 22:06:44 +0800

    Remove todo

--- app/demoroutes.py
+++ app/demoroutes.py
@@ -2,11 +2,10 @@ from typing import Optional
 from fastapi import APIRouter, Response, Depends, Cookie
 from fastapi.security import OAuth2PasswordBearer
 
+from icecream import ic
 from .settings import settings as s
 from .auth.auth import fapiuser, current_user
 
-
-ACCESS_TOKEN_DEMO = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMmNhMzYxNWEtNTE2Yi00NWI1LWEzNGItZjdmZDU3YTZlMmI3IiwiYXVkIjoiZmFzdGFwaS11c2VyczphdXRoIiwiZXhwIjoxNjQ4MjgyMTM4fQ.cd-USTJbR-aZ9xTmQmTQFl8Qd48zk9KeFZRsF3uw57g'
 
 demorouter = APIRouter()
 authonly = OAuth2PasswordBearer(tokenUrl='token')
@@ -17,6 +16,7 @@ def page_demo(response: Response):
 
 
 @demorouter.get('/private', dependencies=[Depends(current_user)])
-async def private(access_token: str = Depends(authonly),
-                  refresh_token: Optional[str] = Cookie(None)):
+# async def private(access_token: str = Depends(authonly),
+#                   refresh_token: Optional[str] = Cookie(None)):
+async def private():
     return 'private'
