8c931fd56692372f0c56e48592ff5d1f2035dc12
enchance <enchance@gmail.com>
2021-04-15 23:03:45 +0800

    Active manager checks for is_active, deleted_at

--- app/auth/models/manager.py
+++ app/auth/models/manager.py
@@ -4,4 +4,10 @@ from tortoise.queryset import QuerySet
 
 class Active(Manager):
     def get_queryset(self) -> QuerySet:
-        return QuerySet(self._model).filter(is_active=True)
+        qs = super().get_queryset()
+        if hasattr(self._model, "deleted_at"):
+            qs = qs.filter(deleted_at=None)
+        if hasattr(self._model, "is_active"):
+            qs = qs.filter(is_active=True)
+        return qs
+        # return QuerySet(self._model).filter(is_active=True)
