bc3ce40712de68f06734aeb8a981caa91332ffd7
enchance <enchance@gmail.com>
2021-05-22 17:01:57 +0800

    Add missing m2m fields to UserMod

--- app/authentication/models/account.py
+++ app/authentication/models/account.py
@@ -35,6 +35,11 @@ class UserMod(DTMixin, TortoiseBaseUserModel):
     website = fields.CharField(max_length=20, default='')
     last_login = fields.DatetimeField(null=True)
 
+    groups = fields.ManyToManyField('models.Group', related_name='group_users',
+                                    through='auth_user_groups', backward_key='user_id')
+    permissions = fields.ManyToManyField('models.Permission', related_name='permission_users',
+                                         through='auth_user_permissions', backward_key='user_id')
+
     full = Manager()
 
     class Meta:
